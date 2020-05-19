4f5e33ead28397935d48062f905054636ab2f8a2
Kala N S <kala001@gmail.com>
2020-05-18 21:43:01 -0600

    Initial commit

---
+++ src/main/java/io/javabrains/ratingsdataservice/resources/RatingsResource.java
@@ -0,0 +1,32 @@
+package io.javabrains.ratingsdataservice.resources;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.javabrains.ratingsdataservice.models.Rating;
+import io.javabrains.ratingsdataservice.models.UserRating;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+	
+	@RequestMapping("/{movieId}")
+	public Rating getRating(@PathVariable("movieId") String movieId) {
+		return new Rating(movieId, 4);
+	}
+	
+	@RequestMapping("users/{userId}")
+	public UserRating getMovies(@PathVariable("userId") String userId) {
+		
+		List<Rating> ratings = Arrays.asList(new Rating("501", 4),
+				new Rating("502", 3));
+		UserRating userrating = new UserRating();
+		userrating.setUserRating(ratings);
+		return userrating;
+	}
+
+}
